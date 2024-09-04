2c2ab6e4b733f304b1a10c092678a3af74a75ee4
Jacob <jacobmannsclareus@gmail.com>
2024-09-04 13:40:32 +0200

    Organiserar NoccoCounter projekt in i egen mapp

---
+++ NoccoCounter/NoccoCounter/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hur mycket tjänar du i timmen");
+int timLön = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Vad heter produkten du köpt?");
+string produkt = Console.ReadLine();
+
+Console.WriteLine("Hur många antal av denna produkt har du köpt? ");
+int antal = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Hur mycket kostar den produkt du spenderat pengar på? ; ");
+int kostnad = Convert.ToInt32(Console.ReadLine());
+
+float totalkostnad = antal * kostnad;
+float timmar = totalkostnad / timLön;
+Console.WriteLine("Du behöver jobba " + Math.Round(timmar,2) + " antal timmar för att ha råd med " + antal + " " + produkt + " du köpt.");
+
