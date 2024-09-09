cdb9b26bd0bb5d6114faedf6e7a91c2e12c0e8a2
Jacob <jacobmannsclareus@gmail.com>
2024-09-09 10:03:09 +0200

    Lägger till första version av mina övningar på metoder

---
+++ method_Training/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml;
+
+namespace method_Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //invokar metoden 'Sum' med värdena 10,15
+            int res = Sum(10,15);
+            //skriver ut resultatet till konsol
+            Console.WriteLine($"Resultat = {res}");
+
+            //Invokar metoden 'Greet' med stringen Jacob
+            Greet("Jacob");
+            Greet("Sangeetha");
+            
+            
+            IsEven(10);
+            IsEven(3);
+
+
+            ReverseStrings("HEJSAN");
+
+
+        }
+
+        static string ReverseStrings(string sentence)
+        {
+            char[] myArray = sentence.ToCharArray();
+            Array.Reverse(myArray);
+
+            string reversedSentence = new string (myArray);
+            Console.WriteLine(reversedSentence);
+            return reversedSentence;
+        }
+
+        static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is an even number");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{num} is an odd number");
+                return false;
+            }
+        }
+
+        //skapar 'Greet' metoden med en string parameter
+        static void Greet(string name)
+        {
+            //Skriver ut till konsolen
+            Console.WriteLine($"Hej {name}");
+        }
+
+        //Skapar 'Sum' metod med parametrarna int num1 och int num2
+        static int Sum(int num1, int num2)
+        {
+            //skapar värdet resultat som tilldelas värdet num1 + num2 
+            int result = num1 + num2;
+            return result;//returnerar värdet i result variabel
+        }
+
+
+    }
+}
