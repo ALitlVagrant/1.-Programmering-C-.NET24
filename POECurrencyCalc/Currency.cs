4e2149be47e2feb84a36fdf8952d7767b608b57e
Jacob <jacobmannsclareus@gmail.com>
2024-09-12 13:27:50 +0200

    Lägger till PoeCurrencyCalc första version

---
+++ POECurrencyCalc/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POECurrencyCalc
+{
+    class Currency
+    {
+        double divineInChaos { get; set; }
+        public Currency(double divinePrice)
+        {
+            divineInChaos = divinePrice;
+        }
+        public double DivineToChaosConvert(double amountOfDiv)
+        {
+           return  divineInChaos * amountOfDiv;
+        }
+        public double ChaosToDivineConvert( double amountOfChaos)
+        {
+            return amountOfChaos / divineInChaos;
+        }
+    }
+}
---
+++ POECurrencyCalc/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace POECurrencyCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input current Divine price in Chaos orb!");
+            double priceCheckDiv = double.Parse(Console.ReadLine());
+            Currency currency = new Currency(priceCheckDiv);
+
+            bool myBool = true;
+
+            while (myBool)
+            {
+                MenuOptions();
+                string currencyChoice = Console.ReadLine();
+                if (currencyChoice == "DC")
+                {
+                    Console.WriteLine("Please enter how many Divines you have!");
+                    double numberOfDivines;
+                    while(!double.TryParse(Console.ReadLine(), out numberOfDivines) || numberOfDivines < 0)
+                    {
+                        Console.WriteLine("Wrong input!");
+                    }
+                    double answerDivineInfo = currency.DivineToChaosConvert(numberOfDivines);
+                    Console.WriteLine($"Your {numberOfDivines} are worth {answerDivineInfo} worth of Chaos!");
+
+                }
+                else if (currencyChoice == "CD")
+                {
+                    Console.WriteLine("Please enter how many Chaos you have!");
+                    double numberOfChaos;
+                    while (!double.TryParse(Console.ReadLine(), out numberOfChaos) || numberOfChaos < 0)
+                    {
+                        Console.WriteLine("Wrong input!");
+                    }
+                    double answerChaosInfo = currency.ChaosToDivineConvert(numberOfChaos);
+                    Console.WriteLine($"Your {numberOfChaos} Chaos are worth {answerChaosInfo} worth of Divines");
+                }
+                else if (currencyChoice == "Q")
+                {
+                    Console.WriteLine("Exiting...");
+                    myBool = false;
+                }
+                
+
+            }
+
+        }
+        public static void MenuOptions()
+        {
+            string[] menuOptions = { "What do you want to do?", "Convert Divine to Chaos(DC)", "Convert Chaos to Divine (CD)","To Quit Press (Q)"};
+            foreach (string option in menuOptions)
+            {
+                Console.WriteLine(option);
+            }    
+        }
+    }
+}
